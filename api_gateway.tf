c3d98a5d8fd69a4ef7cdb71f4fa3f50ebb604254
mitchelldavis <mitchell.davis@veriship.com>
2019-02-08 11:30:40 -0600

    Updated the lambda permissions to reference the api instead of the deployment.  This was the source of a lot of problems.

--- api_gateway.tf
+++ api_gateway.tf
@@ -65,5 +65,5 @@ resource "aws_lambda_permission" "apigw" {
 
 	# The /*/* portion grants access from any method on any resource
 	# within the API Gateway "REST API".
-	source_arn = "${aws_api_gateway_deployment.api_gateway_deployment.execution_arn}/*/*"
+	source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 }
